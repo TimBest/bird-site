eb53da5eee9900595e2b5fc13bf0d77ce6fc0bc2
Tim Best <TimBest@users.noreply.github.com>
2024-03-09 16:45:52 -0500

    static site with tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section
+    class="h-screen w-full bg-gradient-to-r from-yellow-200 via-red-300 to-pink-300 text-center"
+>
+    <h1 class="pt-10 text-7xl">Backyard Beacon Birding</h1>
+    <div class="pt-12 font-serif text-2xl">Ciao!</div>
+</section>
