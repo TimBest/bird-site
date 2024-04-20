9d05717e9639622496982d03de14ed9fcc918528
Tim Best <TimBest@users.noreply.github.com>
2024-04-20 15:43:45 -0400

    add a bird

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
-<section
-    class="h-screen w-full bg-gradient-to-r from-yellow-200 via-red-300 to-pink-300 text-center"
->
-    <h1 class="pt-10 text-7xl">Backyard Beacon Birding</h1>
-    <div class="pt-12 font-serif text-2xl">Ciao!</div>
-</section>
+<header class="min-h-screen bg-white">
+  <nav class="flex items-center py-8 px-14 justify-between">
+    <div class="flex items-center">
+      <h1 class="text-2xl font-bold text-gray-700">Beacon Backyard Birding</h1>
+      <div class="ml-10"></div>
+    </div>
+  </nav>
+  <!-- End Navigation Bar -->
+
+  <!-- Section Hero -->
+  <!-- Box left -->
+  <div class="bg-[#7CA9D8]">
+    <div class="size-1/3 pt-44">
+      <enhanced:img
+        src="/static/IMG_0911.jpg"
+        alt="Tuffed titmouse at a bird feeder"
+      />
+    </div>
+  </div>
+</header>
