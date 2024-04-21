817de53ab99c715cb50851b6e319803167c8a2c9
Tim Best <TimBest@users.noreply.github.com>
2024-04-20 20:48:17 -0400

    add some birds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,10 +11,35 @@
   <!-- Box left -->
   <div class="bg-[#7CA9D8]">
     <div class="size-1/3 pt-44 pl-8 pb-10">
-      <enhanced:img
-        src="/static/IMG_0911.jpg"
-        alt="Tuffed titmouse at a bird feeder"
-      />
+      <enhanced:img src="/static/IMG_0911.jpg" alt="eagle" />
     </div>
+  </div>
+  <div
+    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-8 px-14"
+  >
+    <enhanced:img
+      src="/static/Birds/IMG_0729-Enhanced-NR.jpg"
+      class="rounded-md"
+    />
+    <enhanced:img
+      src="/static/Birds/IMG_0755-Enhanced-NR.jpg"
+      class="rounded-md"
+    />
+    <enhanced:img src="/static/Birds/IMG_0757.jpg" class="rounded-md" />
+    <enhanced:img
+      src="/static/Birds/IMG_0758-Enhanced-NR.jpg"
+      class="rounded-md"
+    />
+    <enhanced:img src="/static/Birds/IMG_0829.jpg" class="rounded-md" />
+    <enhanced:img
+      src="/static/Birds/IMG_0866-Enhanced-NR.jpg"
+      class="rounded-md"
+    />
+    <enhanced:img src="/static/Birds/IMG_0880.jpg" class="rounded-md" />
+    <enhanced:img src="/static/Birds/IMG_0884.jpg" class="rounded-md" />
+    <enhanced:img src="/static/Birds/IMG_0887.jpg" class="rounded-md" />
+    <enhanced:img src="/static/Birds/IMG_0922.jpg" class="rounded-md" />
+    <enhanced:img src="/static/Birds/IMG_9754.jpg" class="rounded-md" />
+    <enhanced:img src="/static/Birds/IMG_9757.jpg" class="rounded-md" />
   </div>
 </header>
