0e737b7b66201da22ad93625897ac6738e38ce68
Tim Best <TimBest@users.noreply.github.com>
2024-05-16 21:04:00 -0400

    enhanced img hack

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,3 @@
-<script>
-  const paths = Object.keys(
-    import.meta.glob("/static/Birds/*.jpg", { query: { enhanced: true } }),
-  ).map((p) => `${p.substring(7)}?url`);
-</script>
-
 <header class="min-h-screen bg-white">
   <nav class="flex items-center py-8 px-14 justify-between">
     <div class="flex items-center">
@@ -23,13 +17,50 @@
   <div
     class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-8 px-14"
   >
-    {#each paths as img}
-      {#if img !== undefined}
-        <img src={img} />
-        <!-- <enhanced:img src={img} class="rounded-md" alt={img} /> -->
-      {:else}
-        <p>undefined</p>
-      {/if}
-    {/each}
+    <!--
+    to list files
+      $ cd static/birds
+      $ for file in *; do echo "<enhanced:img src='/static/Birds/${file}'/>"; done
+    -->
+    <enhanced:img src="/static/Birds/IMG_0729-Enhanced-NR-2.jpg" />
+    <enhanced:img src="/static/Birds/IMG_0755-Enhanced-NR-2.jpg" />
+    <enhanced:img src="/static/Birds/IMG_0758-Enhanced-NR-2.jpg" />
+    <enhanced:img src="/static/Birds/IMG_0829-Enhanced-NR.jpg" />
+    <enhanced:img src="/static/Birds/IMG_0866-Enhanced-NR-2.jpg" />
+    <enhanced:img src="/static/Birds/IMG_0884-2.jpg" />
+    <enhanced:img src="/static/Birds/IMG_0922-2.jpg" />
+    <enhanced:img src="/static/Birds/IMG_0980.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1017.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1075.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1114.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1116.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1153.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1168.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1202-Enhanced-NR.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1221.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1227-Enhanced-NR.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1231.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1237.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1248.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1255.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1263.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1276.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1283.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1312.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1314-Enhanced-NR.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1319.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1380.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1403.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1423.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1439.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1444.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1463.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1474.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1496.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1502.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1509.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1534.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1537.jpg" />
+    <enhanced:img src="/static/Birds/IMG_1562.jpg" />
   </div>
 </header>
